ceabf00100d57fc8ab6cc2b81972561b403fe84b
Roman <gobkins@gmail.com>
2019-11-20 16:10:47 -0500

    death and fall

--- script.js
+++ script.js
@@ -1,39 +1,82 @@
 const duck = document.querySelector('.duck');
 const screenWidth = document.querySelector('body').clientWidth;
-const screenHeight = document.querySelector('body').clientHeight;
-let pos = 0;
+const screenHeight   = window.innerHeight
+                    || document.documentElement.clientHeight
+                    || document.body.clientHeight;
+let posX = 0;
+let posY = 0;
 let directionX = true;
+let dead = false;
+
+const allDucks = document.querySelectorAll('div.duck');
+
+const kill = e => {
+    console.log(e.target);
+    const element = e.target;
+    dead = !dead;
+    element.style.background = 'url(./sprites/brown-kill.png)';
+    element.style.backgroundRepeat = 'no-repeat';
+    element.style.animationName = 'none';
+    // need to remove event listener here
+    setTimeout(() => {e.target.style.background = 'url(./sprites/brown-down.png)'}, 1500);
+
+}
+
+allDucks.forEach( duck => {
+    duck.addEventListener('click', kill);
+});
+
+const fall = (id) => {
+    const element = document.getElementById(id);
+    posY = element.offsetHeight;
+    fallInterval = setInterval(frame);
+    function frame(){
+        posY++;
+        element.style.top = posY + 'px';
+
+        if (posY === (screenHeight - 100)){
+            element.remove();
+        }
+    }
+}
 
 const move = (id, speed) => {
     const element = document.getElementById(id);
     element.style.transform = 'scaleX(1)';
-    setInterval(frame, speed);
+    interval = setInterval(frame, speed);
     function frame(){
         if(directionX){
-            pos++;
-            element.style.left = pos + 'px';
+            posX++;
+            element.style.left = posX + 'px';
             switchDirection(id);
         }
+
         if(!directionX){
-            pos--;
-            element.style.left = pos + 'px';
+            posX--;
+            element.style.left = posX + 'px';
             switchDirection(id);
+        }
+
+        if(dead){
+            clearInterval(interval);
+            element.style.left = posX + 'px';
+            setTimeout(()=> {fall(id)}, 1500);
         }
     }
 }
 
 const switchDirection = (id) =>{
     const element = document.getElementById(id);
-    if(pos === screenWidth){
+    if(posX === screenWidth){
         element.style.transform = 'scaleX(-1)';
         directionX = !directionX;
     }
-    if (pos === 0){
+    if (posX === 0){
         element.style.transform = 'scaleX(1)';
         directionX = !directionX;
     }
 }
 
 // move('duck-1', 10);
-// move('duck-2', 5);
-// move('duck-3', 20);
+// move('duck-2', 20);
+move('duck-3', 30);
